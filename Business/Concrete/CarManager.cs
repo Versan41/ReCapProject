5b803d1669e3a3422cdb8a996412ea3aa103d255
Bahadır Versan <versan412010@hotmail.com>
2021-02-22 02:58:28 +0300

    21.02.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,11 +24,9 @@ namespace Business.Concrete
         }
 
         public IResult Add(Car car)
-        {
-            if (car.BrandName.Length < 2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
+        {           
+            ValidationTool.Validate(new CarValidator(), car);
+
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.BrandName).NotEmpty();
+            RuleFor(c => c.BrandName).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(10).When(c => c.BrandId == 1);
+            RuleFor(c => c.BrandName).Must(StartWithN).WithMessage("Araba adları N harfi ile başlamalı");
+        }
+
+        private bool StartWithN(string arg)
+        {
+            return arg.StartsWith("N");
+        }
+    }
+}
